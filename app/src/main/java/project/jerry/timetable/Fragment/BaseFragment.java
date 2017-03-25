4aa856fe82b2f8765eb01e74f4ae67acd4c85834
JerryMig <jerry.c@mig.me>
2017-03-25 12:34:29 +0800

    Adding main tablayout and pagerview / indicator and snackbar animation

---
+++ app/src/main/java/project/jerry/timetable/Fragment/BaseFragment.java
@@ -0,0 +1,33 @@
+package project.jerry.timetable.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Migme_Jerry on 2017/3/24.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    protected Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        mContext = context;
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getLayoutId(), container, false);
+    }
+
+    protected abstract int getLayoutId();
+
+}
---
+++ app/src/main/java/project/jerry/timetable/Fragment/TrainTimeTableFragment.java
@@ -0,0 +1,67 @@
+package project.jerry.timetable.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import project.jerry.timetable.R;
+
+/**
+ * Created by Migme_Jerry on 2017/3/24.
+ */
+
+public class TrainTimeTableFragment extends BaseFragment {
+
+    private View mIndicator;
+    private Snackbar mSnackbar;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_train_timetable;
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+        mIndicator = (View) view.findViewById(R.id.indicator_line);
+        animateIndicator();
+        makeSnackBar(view);
+    }
+
+    private void animateIndicator() {
+        Animation anim = AnimationUtils.loadAnimation(mContext, R.anim.slide_in_and_out);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (mSnackbar != null) {
+                    mSnackbar.show();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mIndicator.startAnimation(anim);
+    }
+
+    private void makeSnackBar(View view) {
+        mSnackbar = Snackbar.make(view, "hold and press here for menu", Snackbar.LENGTH_INDEFINITE);
+        View snackBarView = mSnackbar.getView();
+        snackBarView.setBackgroundColor(getResources().getColor(R.color.action_bar));
+        snackBarView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSnackbar.dismiss();
+            }
+        });
+    }
+}
